108c518bb347ff39a1f74175fbbbc6150d3e2e71
Jim Daehn <jdaehn@missouristate.edu>
2025-08-19 17:44:16 -0500

    Initial commit

---
+++ src/main/cpp/demo.cpp
@@ -0,0 +1,19 @@
+/**
+ * CSC232 - Data Structures
+ * Missouri State University, Fall 2025
+ *
+ * @file    demo.cpp
+ * @author  Jim Daehn <jdaehn@missouristate.edu>
+ * @brief   Entry-point for demo target.
+ * @version 1.0.0
+ * @date    01/01/2025
+ *
+ * @copyright Copyright (c) 2025 James R. Daehn
+ */
+
+#include "csc232.h"
+
+auto main( ) -> int
+{
+    return EXIT_SUCCESS;
+}
---
+++ src/main/cpp/main.cpp
@@ -0,0 +1,19 @@
+/**
+ * CSC232 - Data Structures
+ * Missouri State University, Fall 2025
+ *
+ * @file    main.cpp
+ * @author  Jim Daehn <jdaehn@missouristate.edu>
+ * @brief   Entry-point for main target.
+ * @version 1.0.0
+ * @date    01/01/2025
+ *
+ * @copyright Copyright (c) 2025 James R. Daehn
+ */
+
+#include "csc232.h"
+
+auto main( ) -> int
+{
+    return EXIT_SUCCESS;
+}
