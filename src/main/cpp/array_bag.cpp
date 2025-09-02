3817995cb56f624bc3ec480c2389de35a08816e4
Jim Daehn <jdaehn@missouristate.edu>
2025-09-02 01:44:55 -0500

    Lecture 5 (#6)
    * Set agenda for lecture
    
    * Implement array bag core methods
    
    * Implementing more array-based methods
    
    * Using trailing return type on get_index_of
    
    * Using trailing return type on get_index_of

---
+++ src/main/cpp/array_bag.cpp
@@ -0,0 +1,114 @@
+//
+// Created by jrd2112 on 9/2/2025.
+//
+
+#include "array_bag.h"
+#include <string>
+
+template < typename item_type >
+auto csc232::array_bag< item_type >::get_index_of( const item_type &target ) const -> int
+{
+    auto found{ false };
+    auto result{ -1 };
+    auto search_index{ 0 };
+    // if bag is empty, item_count_ is zero, so loop is skipped
+    while ( !found && ( search_index < item_count_ ) )
+    {
+        found = items_[ search_index ] == target;
+        if ( found )
+        {
+            result = search_index;
+        }
+        else
+        {
+            ++search_index;
+        }
+    }
+    return result;
+}
+
+template < typename item_type >
+csc232::array_bag< item_type >::array_bag( ) : item_count_{ 0 }, max_items_{ DEFAULT_CAPACITY }
+{
+}
+
+template < typename item_type >
+csc232::array_bag< item_type >::~array_bag( ) = default;
+
+template < typename item_type >
+auto csc232::array_bag< item_type >::get_current_size( ) const -> int
+{
+    return item_count_;
+}
+
+template < typename item_type >
+auto csc232::array_bag< item_type >::is_empty( ) const -> bool
+{
+    return item_count_ == 0;
+}
+
+template < typename item_type >
+auto csc232::array_bag< item_type >::add( const item_type &some_item ) -> bool
+{
+    const auto has_room_to_add{ item_count_ < max_items_ };
+    if ( has_room_to_add )
+    {
+        items_[ item_count_ ] = some_item;
+        item_count_++;
+    }
+    return has_room_to_add;
+}
+
+template < typename item_type >
+auto csc232::array_bag< item_type >::remove( const item_type &some_item ) -> bool
+{
+    auto located_index{ get_index_of( some_item ) };
+    auto can_remove_item{ !is_empty( ) && ( located_index > -1 ) };
+    if ( can_remove_item )
+    {
+        item_count_--;
+        items_[ located_index ] = items_[ item_count_ ];
+    }
+    return can_remove_item; // stub
+}
+
+template < typename item_type >
+auto csc232::array_bag< item_type >::clear( ) -> void
+{
+    item_count_ = 0;
+}
+
+template < typename item_type >
+auto csc232::array_bag< item_type >::get_frequency_of( const item_type &target ) const -> int
+{
+    auto frequency{ 0 };
+    for ( auto current_item : items_ )
+    {
+        if ( current_item == target )
+        {
+            frequency++;
+        }
+    }
+    return frequency;
+}
+
+template < typename item_type >
+auto csc232::array_bag< item_type >::contains( const item_type &target ) const -> bool
+{
+    return get_frequency_of( target ) > 0;
+}
+
+template < typename item_type >
+auto csc232::array_bag< item_type >::to_vector( ) const -> std::vector< item_type >
+{
+    std::vector< item_type > result;
+    for ( auto current_entry : items_ )
+    {
+        result.push_back( current_entry );
+    }
+    return result;
+}
+
+template class csc232::array_bag< char >;
+template class csc232::array_bag< int >;
+template class csc232::array_bag< std::string >;
---
+++ src/main/cpp/array_bag_demo.cpp
@@ -0,0 +1,50 @@
+//
+// Created by jrd2112 on 9/2/2025.
+//
+
+#include "array_bag.h"
+#include "bag.h"
+#include <cstdlib>
+#include <iostream>
+
+void display_bag( const csc232::bag< std::string > &bag )
+{
+    std::cout << "The bag contains " << bag.get_current_size( ) << " items." << std::endl;
+    auto bag_items = bag.to_vector( );
+    for ( auto current_entry : bag_items )
+    {
+        std::cout << current_entry << " ";
+    }
+    std::cout << std::endl
+              << std::endl;
+}
+
+void bag_tester( csc232::bag< std::string > &bag )
+{
+    std::cout << "is_empty returns: " << std::boolalpha << bag.is_empty( );
+    std::cout << "; should be true" << std::endl;
+    display_bag( bag );
+    std::string test_list[] = { "one", "two", "three", "four", "five", "one" };
+    for ( auto current_item : test_list )
+    {
+        bag.add( current_item );
+    }
+    display_bag( bag );
+    std::cout << "is_empty returns: " << std::boolalpha << bag.is_empty( );
+    std::cout << "; should be false" << std::endl;
+    std::cout << "get_current_size( ): " << bag.get_current_size( );
+    std::cout << "; should be 6" << std::endl;
+    std::cout << "Try to add another entry: add(\"extra\") returns ";
+    std::cout << std::boolalpha << bag.add( "extra" ) << std::endl;
+}
+
+auto main( ) -> int
+{
+    auto bag{ csc232::array_bag< std::string >( ) };
+    std::cout << "Testing the array-based bag: " << std::endl;
+    std::cout << "The initial bag is empty" << std::endl;
+    bag_tester( bag );
+    std::cout << "All done!" << std::endl;
+
+    return EXIT_SUCCESS;
+}
